7cbbfcedcbb57305a6ef35bf89d9f8c11a9430ab
L.L. Apsara Hansani <apsliyanage99@gmail.com>
2023-01-29 22:54:39 +0530

    new commit

--- app/controllers/Customer.php
+++ app/controllers/Customer.php
@@ -6,7 +6,7 @@
 
         public function __construct()
         {
-          $this->userModel = $this->model('Customer_Model');
+          $this->customerModel = $this->model('Customer_Model');
 
           if(!$_SESSION['user_email']){
             redirect('Users/login');
