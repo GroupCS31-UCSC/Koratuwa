cf289f91766e8253679ad693fed6f3cb0fad6a77
Sasindu Udayanga <90015129+sasinduudayanga@users.noreply.github.com>
2023-02-02 20:15:33 +0530

    create customer home page

--- app/views/customer/cus_home.php
+++ app/views/customer/cus_home.php
@@ -1,7 +1,94 @@
 <?php require APPROOT.'/views/include/header.php'; ?>
 <link rel="stylesheet" type="text/css" href="<?php echo URLROOT; ?>/public/css/customer/cus_home.css">
+<head><meta name="viewport" content="width=device-width, initial-scale=1.0"></head>
+<!-- <header>
+    <div class="topbar">
+        <img class="logo_topbar" src="<?php echo URLROOT; ?>/img/koratuwa.png" alt="logo">
+        <img src="<?php echo URLROOT; ?>/img/sasindu.jpg" alt="profile_pic">
 
+    </div>
+</header> -->
+<section class="all_products">
+    <div class="container">
+        <div class="feature">
+            <a href="<?php echo URLROOT; ?>/customer/view_product"><img class="pic" src="<?php echo URLROOT; ?>/img/customer/milkbottel.png" alt=""></a>
+            <div class="fresh_milk_about">
+                <p><h1>Fresh Milk</h1>
+                milk,asdda,asdfad <br><b>RS. 100</b></p>
+            </div>
+        </div>
+        <div class="feature">
+            <img class="pic" src="<?php echo URLROOT; ?>/img/customer/milkbottel.png" alt="">
+            <div class="fresh_milk_about">
+                <p><h1>Fresh Milk</h1>
+                milk,asdda,asdfad <br><b>RS. 100</b></p>
+            </div>
+        </div>
+        <div class="feature">
+            <img class="pic" src="<?php echo URLROOT; ?>/img/customer/milkbottel.png" alt="">
+            <div class="fresh_milk_about">
+                <p><h1>Fresh Milk</h1>
+                milk,asdda,asdfad <br><b>RS. 100</b></p>
+            </div>
+        </div>
+        <div class="feature">
+            <img class="pic" src="<?php echo URLROOT; ?>/img/customer/milkbottel.png" alt="">
+            <div class="fresh_milk_about">
+                <p><h1>Fresh Milk</h1>
+                milk,asdda,asdfad <br><b>RS. 100</b></p>
+            </div>
+        </div>
+        <div class="feature">
+            <img class="pic" src="<?php echo URLROOT; ?>/img/customer/milkbottel.png" alt="">
+            <div class="fresh_milk_about">
+                <p><h1>Fresh Milk</h1>
+                milk,asdda,asdfad <br><b>RS. 100</b></p>
+            </div>
+        </div>
+        <div class="feature">
+            <img class="pic" src="<?php echo URLROOT; ?>/img/customer/milkbottel.png" alt="">
+            <div class="fresh_milk_about">
+                <p><h1>Fresh Milk</h1>
+                milk,asdda,asdfad <br><b>RS. 100</b></p>
+            </div>
+        </div>                                              
+    </div>
+</section>
+<section class="fresh_milk">
+    <div class="container">
+            <div class="feature1">
+                <div class="milk_img">
+                    <img class="pic_milk" src="<?php echo URLROOT; ?>/img/customer/milkbottel.png" alt="">
+                </div>
+            </div>
+            <div class="feature1">
+                <div class="milk_content">
+                    <h1>Koratuwa Fresh Milk</h1>
+                    We are proud to offer a wide variety of products from our farmstead creamery. We have a unique opportunity to offer two lines of milk, organic and traditional, both bottled separately, at our creamery. Our products are made only with our farm-fresh, rBGH-free milk that we produce on our farms. You’ll taste the Local Difference! <br>
 
+                    Organic Milk <br>
+                    Sizes: Gallon | Half Gallon <br>
+                    Types: Skim* | 1% | 2% | Whole <br><br>
+
+                    Traditional Milk <br>
+                    Sizes: Gallon | Half Gallon | Quarts | Pints <br>
+                    Types: Skim | 1% | 2% | Whole | Chocolate | 1% Chocolate <br><br>
+
+                    Lactose Free Milk <br>
+                    Sizes: Gallon | Half Gallon <br>
+                    Types: 2% | Whole <br><br>
+
+                    Also available <br>
+                    Heavy cream* | Half and half* <br>            
+                </div>
+            </div>            
+    </div>
+</section>
+<section class="yourget">
+    <div class="container">
+        <div></div>
+    </div>
+</section>
 
 
 <?php require APPROOT.'/views/include/footer.php'; ?>
---
+++ app/views/customer/view_product.php
@@ -0,0 +1,11 @@
+<?php require APPROOT.'/views/include/header.php'; ?>
+<link rel="stylesheet" type="text/css" href="<?php echo URLROOT; ?>/public/css/customer/cus_home.css">
+<head><meta name="viewport" content="width=device-width, initial-scale=1.0"></head>
+
+
+
+
+
+
+<?php require APPROOT.'/views/include/footer.php'; ?>
+<script src="<?php echo URLROOT; ?>/js/customer.js"></script>
