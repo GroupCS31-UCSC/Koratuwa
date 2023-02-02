45c2c044674e214be9966198a9d18783f0c9d302
L.L. Apsara Hansani <apsliyanage99@gmail.com>
2023-02-02 07:58:08 +0530

    Update admin.js

--- public/js/admin.js
+++ public/js/admin.js
@@ -1,60 +1,59 @@
-
 // //charts - Admin Dashboard page
-var profit= document.getElementById('profit').getContext('2d');
+var profit = document.getElementById('profit').getContext('2d');
 
-var myChart= new Chart(profit, {
-  type: 'doughnut',
-  data: {
-    labels: ['Milk Packets', 'Yogurts', 'Cheese', 'Fresh Milk'],
-    datasets: [{
-      label: 'profit by Productions',
-      data: [32000,29000,18000,41000],
-      backgroundColor: [
-        'rgba(255, 99, 132, 0.2)',,
-        'rgba(255, 206, 86, 0.2)',
-        'rgba(75, 192, 192, 0.2)',
-        'rgba(153, 102, 255, 0.2)',
-      ],
-      borderColor: [
-        'rgba(255, 99, 132, 1)',
-        'rgba(255, 206, 86, 1)',
-        'rgba(75, 192, 192, 1)',
-        'rgba(153, 102, 255, 1)',
-      ],
-      borderWidth:0.5
-    }]
-  },
-  options: {
-    responsive: true,
+var myChart = new Chart(profit, {
+    type: 'doughnut',
+    data: {
+        labels: ['Milk Packets', 'Yogurts', 'Cheese', 'Fresh Milk'],
+        datasets: [{
+            label: 'profit by Productions',
+            data: [32000, 29000, 18000, 41000],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)', ,
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+            ],
+            borderWidth: 0.5
+        }]
+    },
+    options: {
+        responsive: true,
 
-  }
+    }
 });
 
-var totProfit= document.getElementById('earning').getContext('2d');
-var myChart= new Chart(totProfit, {
-  type: 'bar',
-  data: {
-    labels: ['June', 'July', 'August', 'September', 'October', 'November'],
-    datasets: [{
-      label: 'Total profit',
-      data: [320000,209000,108000,401000,120040,380000],
-      backgroundColor: [
-        'rgba(255, 99, 132, 0.2)',,
-        'rgba(255, 206, 86, 0.2)',
-        'rgba(75, 192, 192, 0.2)',
-        'rgba(153, 102, 255, 0.2)',
-      ],
-      borderColor: [
-        'rgba(255, 99, 132, 1)',
-        'rgba(255, 206, 86, 1)',
-        'rgba(75, 192, 192, 1)',
-        'rgba(153, 102, 255, 1)',
-      ],
-      borderWidth:0.5
-    }]
-  },
-  options: {
-    responsive: true,
+var totProfit = document.getElementById('earning').getContext('2d');
+var myChart = new Chart(totProfit, {
+    type: 'bar',
+    data: {
+        labels: ['June', 'July', 'August', 'September', 'October', 'November'],
+        datasets: [{
+            label: 'Total profit',
+            data: [320000, 209000, 108000, 401000, 120040, 380000],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)', ,
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+            ],
+            borderWidth: 0.5
+        }]
+    },
+    options: {
+        responsive: true,
 
-  }
+    }
 });
