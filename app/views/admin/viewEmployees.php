1dc78b4b34630bc8d4ec8cc78f6698fa7e028315
Sasindu Udayanga <90015129+sasinduudayanga@users.noreply.github.com>
2023-02-03 18:19:50 +0530

    admin, table make responsive design

--- app/views/admin/viewEmployees.php
+++ app/views/admin/viewEmployees.php
@@ -8,46 +8,50 @@
 <?php flash('addEmp_flash') ?>
 <?php flash('updateEmp_flash') ?>
 <?php flash('dltEmp_flash') ?>
+<section>
+  <div class="container" style="overflow-x: auto;">
 
-<table>
-  <tr>
-    <th>Employee Id</th>
-    <th>Name</th>
-    <th>Employment</th>
-    <!-- <th>Salary</th> -->
-    <th>NIC</th>
-    <th>Contact Number</th>
-    <th>Gender</th>
-    <!-- <th>Age</th> -->
-    <th>Email</th>
-    <!--<th>Address</th>-->
-    <!-- <th>Last Accessed On</th> -->
-    <th>Action</th>
-  </tr>
+    <table>
+      <tr>
+        <th>Employee Id</th>
+        <th>Name</th>
+        <th>Employment</th>
+        <!-- <th>Salary</th> -->
+        <th>NIC</th>
+        <th>Contact Number</th>
+        <th>Gender</th>
+        <!-- <th>Age</th> -->
+        <th>Email</th>
+        <!--<th>Address</th>-->
+        <!-- <th>Last Accessed On</th> -->
+        <th>Action</th>
+      </tr>
 
-  <?php foreach ($data['empView'] as $emp) : ?>
-  <tr>
-    <td><?php echo $emp->employee_id; ?></td>
-    <td><?php echo $emp->employee_name; ?></td>
-    <td><?php echo $emp->employment; ?></td>
-    <!-- <td><?php echo $emp->salary; ?></td> -->
-    <td><?php echo $emp->nic; ?></td>
-    <td><?php echo $emp->contact_number; ?></td>
-    <td><?php echo $emp->gender; ?></td>
-    <td><?php echo $emp->email; ?></td>
-    <!-- <td><?php echo ''; ?></td>    age -->
-    <!-- <td><?php echo ''; ?></td> -->
-    <td>
-      <div class="table-btns">
-      <a href="<?php echo URLROOT?>/Admin/updateEmployees/<?php echo $emp->email ?>"><button class="updateBtn">UPDATE</button></a>
-      <a href="<?php echo URLROOT?>/Admin/deleteEmployees/<?php echo $emp->email ?>"><button class="deleteBtn" onclick="delete()">DELETE</button></a>
-    </div>
-    </td>
-  </tr><br>
-  <?php endforeach; ?>
+      <?php foreach ($data['empView'] as $emp) : ?>
+      <tr>
+        <td><?php echo $emp->employee_id; ?></td>
+        <td><?php echo $emp->employee_name; ?></td>
+        <td><?php echo $emp->employment; ?></td>
+        <!-- <td><?php echo $emp->salary; ?></td> -->
+        <td><?php echo $emp->nic; ?></td>
+        <td><?php echo $emp->contact_number; ?></td>
+        <td><?php echo $emp->gender; ?></td>
+        <td><?php echo $emp->email; ?></td>
+        <!-- <td><?php echo ''; ?></td>    age -->
+        <!-- <td><?php echo ''; ?></td> -->
+        <td>
+          <div class="table-btns">
+          <a href="<?php echo URLROOT?>/Admin/updateEmployees/<?php echo $emp->email ?>"><button class="updateBtn">UPDATE</button></a>
+          <a href="<?php echo URLROOT?>/Admin/deleteEmployees/<?php echo $emp->email ?>"><button class="deleteBtn" onclick="delete()">DELETE</button></a>
+        </div>
+        </td>
+      </tr><br>
+      <?php endforeach; ?>
 
-</table>
+    </table>
 
+  </div>
+</section>
 
 <input type="button" value="Add Employee" class="addBtn" onclick="location.href='<?php echo URLROOT; ?>/Admin/addEmployees' ">
 <!--
