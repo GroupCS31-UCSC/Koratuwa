eb7bcb816645a489aede1748b43aaee72f57453c
Kawya Sandamini <2020cs085@stu.ucsc.cmb.ac.lk>
2023-02-05 11:18:02 +0530

    Create cashier dashboard, home page

--- app/controllers/Cashier.php
+++ app/controllers/Cashier.php
@@ -1,11 +1,8 @@
 <?php
-
-    class Cashier extends Controller
-    {
+    class Cashier extends Controller {
       public $cashierModel;
 
-        public function __construct()
-        {
+        public function __construct() {
           $this->cashierModel = $this->model('Cashier_Model');
 
           if(!$_SESSION['user_email']){
@@ -13,7 +10,10 @@
           }          
         }
 
-        #cashierHome
+        public function cashierHome() {
+          $data = [];
+          $this->view('Cashier/cashier_home',$data);
+        }
 
     }
 
