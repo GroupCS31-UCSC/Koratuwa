55c924891ccde964a303e05092e20ce35dd80310
Kawya Sandamini <2020cs085@stu.ucsc.cmb.ac.lk>
2023-02-09 21:44:34 +0530

    Updated pages in cashier

--- app/views/cashier/cashier_home.php
+++ app/views/cashier/cashier_home.php
@@ -10,18 +10,47 @@
       <div>
         <div class="cardName">Online Orders</div>
         <div class="numbers">100</div>
+        <div class="day"><div class="circle"></div><label for="">Pending</label></div>
+      </div>
+    </div>
+    <div class="card">
+      <div>
+        <div class="cardName">Online Orders</div>
+        <div class="numbers">30</div>
+        <div class="day"><div class="circle"></div><label for="">Delivered</label></div>
+      </div>
+    </div>
+    <div class="card">
+      <div>
+        <div class="cardName">Online Orders</div>
+        <div class="numbers">90</div>
+        <div class="day">
+          <div class="circle"></div>
+          <label for="">Processing</label>
+        </div>
       </div>
     </div>
     <div class="card">
       <div>
         <div class="cardName">Onsite Sales</div>
         <div class="numbers">30</div>
+        <div class="day"><div class="circle"></div><label for="">For Today</label></div>
       </div>
     </div>
   </div>
   
 </session>
-
+<!-- Add chart -->
+<div class="graphbox">
+  <div class="box">
+    <label><center>Orders</center></label>
+    <canvas id="order"></canvas>
+  </div>
+  <div class="box">
+    <label><center>Percentages of sales</center></label>
+    <canvas id="type"></canvas>
+  </div>
+</div>
 
 <?php require APPROOT.'/views/include/footer.php'; ?>
 <script src="<?php echo URLROOT; ?>/js/cashier.js"></script>
--- app/views/cashier/updateSale.php
+++ app/views/cashier/updateSale.php
@@ -10,18 +10,18 @@
 	<br>
 	<form action="<?php echo URLROOT; ?>/Cashier/addSale" method="POST">
         <!-- Product Id -->
-        <div class="form-input-title">Product Id</div>
-        <input type="text" name="product_id" id="product_id" class="product_id" value="#">
+        <div class="form-input-title">Sale Id</div>
+        <input type="text" name="product_id" id="product_id" class="product_id" value="" disabled="disabled">
         <!-- Sales income -->
         <!-- <div class="form-input-title">Sales income</div>
         <input type="text" name="sales_income" id="sales_income" class="sales_income" value="#"> -->
 		<!--Customer name-->
 		<div class="form-input-title">Customer Name</div>
-        <input type="text" name="customer_name" id="customer_name" class="customer_name" value="#">
+        <input type="text" name="customer_name" id="customer_name" class="customer_name" value="">
         
         <!--Contact no-->
         <div class="form-input-title">Contact no</div>
-        <input type="text" name="contact_no" id="contact_no" class="contact_no" value="#">
+        <input type="text" name="contact_no" id="contact_no" class="contact_no" value="">
     
 		<br>
 		<input type="submit" value="Submit" class="submitBtn">
