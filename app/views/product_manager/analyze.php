525473d1a42ac586482e8b6896e896eeae3aeef1
Dewmini-Vithanage <2020cs194@stu.ucsc.cmb.ac.lk>
2023-02-19 12:43:33 +0530

    Add stock changed to PDO

---
+++ app/views/product_manager/analyze.php
@@ -0,0 +1,20 @@
+<link rel="stylesheet" type="text/css" href="<?php echo URLROOT; ?>/public/css/product_manager/productCategories.css">
+<?php require APPROOT.'/views/include/header.php'; ?>
+<?php require APPROOT.'/views/product_manager/pm_dashboard.php'; ?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<?php require APPROOT.'/views/include/footer.php'; ?>
