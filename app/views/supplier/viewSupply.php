f636f81b708612e3a72fb2ef14237eb0226f8fd8
Sasindu Udayanga <90015129+sasinduudayanga@users.noreply.github.com>
2023-02-20 17:55:10 +0530

    viewSupply popup form created

--- app/views/supplier/viewSupply.php
+++ app/views/supplier/viewSupply.php
@@ -54,7 +54,8 @@
                 </div>
               <?php else : ?>
                 <div class="table-btns">
-                  <a href="<?php echo URLROOT?>/Supplier/updateSupOrder/<?php echo $supOrd->supply_order_id ?>"><button class="updateBtn">View</button></a>
+                  <!-- <a href="<?php echo URLROOT?>/Supplier/updateSupOrder/<?php echo $supOrd->supply_order_id ?>"><button class="updateBtn">View</button></a> -->
+                  <button id="myBtn" class="updateBtn">View Invoice</button>
                 </div>
               <?php endif; ?>
             </td>
@@ -99,22 +100,59 @@
 
 <p><?php echo($data['ordSum']) ?></p>     <!-- display the total milk quantity of relavant supplier -->
 
+
+  <!-- Modal content -->
 <!-- Trigger/Open The Modal -->
-<button id="myBtn">Open Modal</button>
+<!-- <button id="myBtn" >Open modal</button> -->
+
 <!-- The Modal -->
 <div id="myModal" class="modal">
 
   <!-- Modal content -->
   <div class="modal-content">
-    <span class="close">&times;</span>
-    <p>Some text in the Modal..</p>
+    <div class="modal-header">
+      <span class="close">&times;</span>
+      <h2>Modal Header</h2>
+    </div>
+    <div class="modal-body">
+      <p>Some text in the Modal Body</p>
+      <p>Some other text...</p>
+    </div>
+    <div class="modal-footer">
+      <h3>Modal Footer</h3>
+    </div>
   </div>
 
 </div>
-  <!-- </div>
+<script>
+  //--------------Model form---------------------------//
+  // Get the modal
+  var modal = document.getElementById("myModal");
 
-</div>
-</div> -->
+  // Get the button that opens the modal
+  var btn = document.getElementById("myBtn");
+
+  // Get the <span> element that closes the modal
+  var span = document.getElementsByClassName("close")[0];
+
+  // When the user clicks the button, open the modal 
+  btn.onclick = function() {
+    modal.style.display = "block";
+  }
+
+  // When the user clicks on <span> (x), close the modal
+  span.onclick = function() {
+    modal.style.display = "none";
+  }
+
+  // When the user clicks anywhere outside of the modal, close it
+  window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  }  
+</script>
+
 <?php require APPROOT.'/views/include/footer.php'; ?>
 <script src="<?php echo URLROOT; ?>/js/supplier.js"></script>
 
