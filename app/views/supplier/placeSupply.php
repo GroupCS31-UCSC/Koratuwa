cb24584db9dda108a9df85bfcac22699098e85e3
Sasindu Udayanga <90015129+sasinduudayanga@users.noreply.github.com>
2023-02-25 06:27:31 +0530

    Supplier home page modified

--- app/views/supplier/placeSupply.php
+++ app/views/supplier/placeSupply.php
@@ -13,22 +13,22 @@
 
 			<form action="<?php echo URLROOT; ?>/Supplier/placeSupply" method="POST">
 				<!--quantity-->
-				<div class="form-input-title">Supply Quantity (LITER)</div>
+				<!-- <div class="form-input-title">Supply Quantity (LITER)</div>
 				<span class="form-invalid"><?php echo $data['quantity_err']; ?></span>
-				<input type="text" name="quantity" id="quantity" class="quantity" value="<?php echo $data['quantity']; ?>">
+				<input type="text" name="quantity" id="quantity" class="quantity" value="<?php echo $data['quantity']; ?>"> -->
 
 				<!--supply_date-->
-				<div class="form-input-title">Supply Date</div>
+				<!-- <div class="form-input-title">Supply Date</div>
 				<span class="form-invalid"><?php echo $data['date_err']; ?></span>
-				<input type="date" name="date" id="date" class="date" value="<?php echo $data['date']; ?>">
+				<input type="date" name="date" id="date" class="date" value="<?php echo $data['date']; ?>"> -->
 
 				<!--address-->
-				<div class="form-input-title">Address</div>
+				<!-- <div class="form-input-title">Address</div>
 				<span class="form-invalid"><?php echo $data['address_err']; ?></span>
-				<input type="text" name="address" id="address" class="address" value="<?php echo $data['address']; ?>">
+				<input type="text" name="address" id="address" class="address" value="<?php echo $data['address']; ?>"> -->
 
 				<br>
-				<input type="submit" value="Submit" class="submitBtn"><br>
+				<!-- <input type="submit" value="Submit" class="submitBtn"><br> -->
 			</form>
 
 <?php require APPROOT.'/views/include/footer.php'; ?>
