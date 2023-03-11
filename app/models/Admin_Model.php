50c4227a7b8a8ca82af525a10de2202397e710ae
L.L. Apsara Hansani <apsliyanage99@gmail.com>
2023-03-11 21:30:40 +0530

    fix some mistakes

--- app/models/Admin_Model.php
+++ app/models/Admin_Model.php
@@ -73,9 +73,10 @@
     //add newly registering employee's details
     public function addEmployees($data)
     {
-      $this->db->query('INSERT INTO user(user_id,email,password,user_type) VALUES(:id, :email, :password, :user_type)');
+      $this->db->query('INSERT INTO user(user_id,user_name,email,password,user_type) VALUES(:id, :name, :email, :password, :user_type)');
 			//value binding
 			$this->db->bind(':id', $data['id']);
+      $this->db->bind(':name', $data['name']);
 			$this->db->bind(':email', $data['email']);
 			$this->db->bind(':password', $data['password']);
 			$this->db->bind(':user_type',$data['employment']);
