024a53381162fabb693af52b82d1706c15b5df8a
L.L. Apsara Hansani <apsliyanage99@gmail.com>
2023-04-26 15:53:49 +0530

    Update hosted database details in config.php file

--- app/config/config.php
+++ app/config/config.php
@@ -1,9 +1,14 @@
 <?php
 
 	//Database Configuration
-	define('DB_HOST', 'localhost');
-	define('DB_USER', 'root');
-	define('DB_PASSWORD', '');
+	// define('DB_HOST', 'localhost');
+	// define('DB_USER', 'root');
+	// define('DB_PASSWORD', '');
+	// define('DB_NAME', 'koratuwa');
+
+	define('DB_HOST', 'koratuwa.csjajiu1hjs0.ap-south-1.rds.amazonaws.com');
+	define('DB_USER', 'admin');
+	define('DB_PASSWORD', 'apsara99##');
 	define('DB_NAME', 'koratuwa');
 
 	//APPROOT
