c0c73d0754afc97583d0278a319449c807fb369a
Kawya Sandamini <2020cs085@stu.ucsc.cmb.ac.lk>
2023-04-28 09:51:25 +0530

    modify backend for add sale

--- app/views/cashier/cashier_home.php
+++ app/views/cashier/cashier_home.php
@@ -56,45 +56,97 @@
 		<center><h1>Product Details</h1></center>
 	</div>
 	<br>
-	<form action="<?php echo URLROOT; ?>/Cashier/addSale" method="POST" enctype="multipart/form-data">        
+	<form action="<?php echo URLROOT; ?>/Cashier/addSale" method="POST" enctype="multipart/form-data">  
+    <!-- Product Id -->
     <div class="form-input-title">Product Id</div>
     <span class="form-invalid"><?php echo $data[0]['productId_err']; ?></span>
     <label for="Select the Product"></label>
     <?php $values = $data[1]?>
     <select name="productId" id="productId">
       <?php foreach($values as $product_id):?>
-        <option value="<?=$product_id->product_id?>" name="productId"><?=$product_id->product_id?></option>
+        <option value="<?=$product_id->product_id?>" name="productId" id="productId"><?=$product_id->product_id?></option>
       <?php endforeach;?>
     </select>
-
+    <!-- Product Name -->
+    <!-- <div class="form-input-title">Product Name</div>
+    <input type="text" name="productName" id="productName" class="productName" value="<?php $values = $data[1]?>" disabled> -->
+    
+    <!-- Quantity -->
 		<div class="form-input-title">Quantity</div>
-    <span class="form-invalid"><?php echo $data[0]['quantity_err']; ?></span>
-    <input type="number" name="quantity" id="quantity" class="quantity" value="<?php echo $data[0]['quantity'];?>" required>    
+    <!-- <span class="form-invalid"><?php echo $data[0]['quantity_err']; ?></span> -->
+    <input type="number" name="quantity" id="quantity" class="quantity" value="" required onchange="calculate()">  
+      <!-- price// this should get from table -->
+    <!-- Unit Price -->
+    <div class="form-input-title">Unit Price</div>
+    <!-- <span class="form-invalid"><?php echo $data[0]['unitPrice_err']; ?></span> -->
+    <input type="number" name="unitPrice" id="unitPrice" class="unitPrice" value="" required>
 		<br>
-		<input type="submit" value="OK" class="submitBtn">
+		<input type="submit" value="OK" class="submitBtn" onclick="addData()">
   </form>
   
 </div>
 <table>
       <tr>
-        <th>Product Name</th>
+        <th id="productId">Product Id</th>
+        <!-- <th>Product Name</th> -->
         <th>Unit Price</th>
         <th>Quantity</th>
         <th>Price</th>
       </tr>
-      <tr>
-        <td>Product 1</td>
-        <td>Rs. 100</td>
-        <td>1</td>
-        <td>Rs. 100</td>        
+      <tr id="tbody">
       </tr>
+    </table>
+    <hr>
+    <table>
+      <tr>
+        <th width="20%"></th>
+        <th width="20%"></th>
+        <th width="20%"></th>
+        <th width="20%">Subtotal:</th>
+        <th width="20%" id="subtotal-amount"></th>
+      </tr>
+      <!-- <tr>
+            <th width="20%"></th>
+            <th width="20%"></th>
+            <th width="20%"></th>
+            <th width="20%">Tax:</th>
+            <th width="20%">0 Rs</th>
+        </tr> -->
+        <tr>
+            <th width="20%"></th>
+            <th width="20%"></th>
+            <th width="20%"></th>
+            <th width="20%">Total Amount(Rs):</th>
+            <th width="20%" id="total"></th>
+        </tr>
     </table>
     <!-- </div> -->
   </div>
-</div>
+<!-- </div> -->
 
 
 
 
 <?php require APPROOT.'/views/include/footer.php'; ?>
 <script src="<?php echo URLROOT; ?>/js/cashier.js"></script>
+
+<script>
+  function calculate(){
+    var quantity = document.getElementById("quantity").value;
+    var unitPrice = document.getElementById("unitPrice").value;
+    var price = quantity * unitPrice;
+    document.getElementById("price").value = price;
+  }
+
+  function addData(){
+    document.getElementById("tbody").innerHTML += "<tr><td>" + productId + "</td><td>" + unitPrice + "</td><td>" + quantity + "</td><td>";
+    document.getElementById("productId").value = "";
+    document.getElementById("unitPrice").value = "";
+    document.getElementById("quantity").value = "";
+
+    document.getElementById("subtotal-amount").innerHTML = unitPrice * quantity;
+    document.getElementById("total").innerHTML = unitPrice * quantity;
+
+    var subtotal = 0;
+  }
+</script>
