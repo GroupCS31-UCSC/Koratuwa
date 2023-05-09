1e549d4565e0de7e2c808e6908e1598c9768cfcd
L.L. Apsara Hansani <apsliyanage99@gmail.com>
2023-05-10 01:04:02 +0530

    view products front end created

--- app/views/admin/viewProduction.php
+++ app/views/admin/viewProduction.php
@@ -4,7 +4,40 @@
 <!-- ______________________________________________________________________________________________________-->
 
 
-<section>
+<?php foreach ($data['productionView'] as $pv) : ?>
+
+      <div class="product-card">
+        <div class="product-box">
+          
+          <div class="div0-img">
+            <img src="<?php echo UPLOADS . $pv->image ?>" width='100' height='100'>
+          </div>
+          <div class="div1-name">
+            <strong><span><?php echo $pv->product_id; ?></span></strong><br>
+            <strong><span><?php echo $pv->product_name; ?></span></strong>
+          </div>
+          <div class="div2-details">
+            <strong>Ingredients:</strong><span><?php echo $pv->ingredients; ?></span><br>
+            <strong>Expiry Duration:</strong><span><?php echo $pv->expiry_duration; ?></span><br>
+            <strong>Unit Size:</strong><span><?php echo $pv->unit_size; ?></span><br>
+            <strong>Unit Price:</strong><span><?php echo $pv->unit_price; ?></span><br>
+          </div>
+          <div class="div3-availQty">
+            <span>Available Quantity:<br><?php echo $pv->available_quantity; ?></span>
+          </div>
+          <div class="div4-more">
+            <a href="<?php echo URLROOT?>/Admin/viewProductStock/<?php echo $pv->product_id ?>"><button class="viewBtn" title="View Stock"><i class="fas fa-eye"></i></button></a>
+          </div>
+
+        </div>
+      </div>
+
+<?php endforeach; ?>
+
+
+
+
+<!-- <section>
   <div class="container" style="overflow-x: auto;">
 
     <table>
@@ -39,7 +72,7 @@
 
     </table>
 
-<input type="button" value="Add New production" class="addBtn" onclick="location.href='<?php echo URLROOT; ?>/Admin/addProduction' ">
+<input type="button" value="Add New production" class="addBtn" onclick="location.href='<?php echo URLROOT; ?>/Admin/addProduction' "> -->
 
 
 
