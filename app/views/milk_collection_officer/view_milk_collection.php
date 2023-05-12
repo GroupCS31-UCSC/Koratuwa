f2c4ed7e2a41f4740f24509313c9e53f64801122
L.L. Apsara Hansani <apsliyanage99@gmail.com>
2023-05-12 13:09:18 +0530

    mco modified

--- app/views/milk_collection_officer/view_milk_collection.php
+++ app/views/milk_collection_officer/view_milk_collection.php
@@ -2,6 +2,7 @@
 <link rel="stylesheet" type="text/css" href="<?php echo URLROOT; ?>/public/css/mco/view_milk_collection.css">
 <?php require APPROOT.'/views/milk_collection_officer/mco_dashboard.php';  ?>
 <!-- ______________________________________________________________________________________________________-->
+<script src="<?php echo URLROOT; ?>/js/mco.js"></script>
 
 <div class="container" style="overflow-x: auto;">
 <section>
@@ -10,36 +11,43 @@
 <div class="tableSection">
   <h1>Koratuwa Internal Milk Collection</h1>
 
-  <table>
-    <tr>
-      <th>Collection Id</th>
-      <th>Collected Date</th>
-      <th>Collected Time</th>
-      <th>Stall Id</th>
+  <table id="detailsTable">
+    <thead>
+      <th col-index = 1>Collection Id</th>
+      <th col-index = 2>Collected Date</th>
+      <th col-index = 3>Collected Time</th>
+      <th col-index = 4>
+        <select class="table-filter" onchange="filter_rows()">
+            <option value="all">Stall Id</option>
+        </select></th>
       <th>Quantity(L)</th>
-      <th>More</th>       
-    </tr>
-  <?php $data_index=0 ?>
-  <?php foreach ($data['milkView'] as $mc) : ?>
-    <tr>
-      <td><?php echo $mc->milk_collection_id; ?></td>
-      <td><?php echo $mc->collected_date; ?></td>
-      <td><?php echo $mc->collected_time; ?></td>
-      <td><?php echo $mc->stall_id; ?></td>
-      <td><?php echo $mc->quantity; ?></td>
-      <td>
-      <div class="table-btns">
-        <!-- <a href="<?php echo URLROOT?>/Milk_Collection_Officer/collectionDetails/<?php echo $mc->milk_collection_id ?>"><button class="viewBtn">View</button></a> -->
-        <a href="#"><button class="viewBtn" onclick="openModel1('<?=$mc->milk_collection_id?>')" id="<?php echo($data_index) ?>"><i class="fas fa-eye"></i></button></a>
-      </div>
-      </td>
-    </tr>
-    <?php $data_index++; ?> 
-  <?php endforeach; ?>
-
+      <th>View More</th>       
+    </thead>
+    <tbody>
+      <?php $data_index=0 ?>
+      <?php foreach ($data['milkView'] as $mc) : ?>
+        <tr>
+          <td><?php echo $mc->milk_collection_id; ?></td>
+          <td><?php echo $mc->collected_date; ?></td>
+          <td><?php echo $mc->collected_time; ?></td>
+          <td><?php echo $mc->stall_id; ?></td>
+          <td><?php echo $mc->quantity; ?></td>
+          <td>
+          <div class="table-btns">
+            <!-- <a href="<?php echo URLROOT?>/Milk_Collection_Officer/collectionDetails/<?php echo $mc->milk_collection_id ?>"><button class="viewBtn">View</button></a> -->
+            <a href="#"><button class="viewBtn" onclick="openModel1('<?=$mc->milk_collection_id?>')" id="<?php echo($data_index) ?>"><i class="fas fa-eye"></i></button></a>
+          </div>
+          </td>
+        </tr>
+        <?php $data_index++; ?> 
+      <?php endforeach; ?>
+  </tbody>
   </table>
 </div>
 
+<script>
+  getUniqueValuesFromColumn();
+</script>
 
 <!----------------- popup view ---------------->
 <div class="model fade in" id="model" tabindex="-1">
@@ -63,4 +71,4 @@
     
   
 <?php require APPROOT.'/views/include/footer.php'; ?>
-<script src="<?php echo URLROOT; ?>/js/mco.js"></script>
+
